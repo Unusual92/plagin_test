ed45393e04b35df569737e981b2abb0a00b54b1c
Unusual92 <gadya3009@gmail.com>
2023-10-10 21:13:37 +0600

    first commit

--- src/main/java/e/my_server/food/Food.java
+++ src/main/java/e/my_server/food/Food.java
@@ -1,6 +1,7 @@
 package e.my_server.food;
 
 import co.aikar.commands.BukkitCommandManager;
+import e.my_server.food.listeners.FoodConfig;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Food extends JavaPlugin {
